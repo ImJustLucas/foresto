185fe1b19975c460630de22afa27c49139d03324
ImJustLucas <lucas.bellier@icloud.com>
2025-02-04 11:37:18 +0100

    ✨ add Typography, Avatar, and Dropdown Menu components for enhanced UI

---
+++ components/typography/index.tsx
@@ -0,0 +1,54 @@
+import { cn } from "@/lib/utils";
+
+type TypographyType = {
+  children: React.ReactNode;
+  bold?: boolean;
+};
+
+export const TypographyH3: React.FC<TypographyType> = ({
+  children,
+  bold = true,
+}) => {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl tracking-tight",
+        bold && "font-semibold"
+      )}
+    >
+      {children}
+    </h3>
+  );
+};
+
+export const TypographyH4: React.FC<TypographyType> = ({
+  children,
+  bold = true,
+}) => {
+  return (
+    <h3
+      className={cn(
+        "scroll-m-20 text-xl tracking-tight",
+        bold && "font-semibold"
+      )}
+    >
+      {children}
+    </h3>
+  );
+};
+
+export const TypographyP: React.FC<TypographyType> = ({
+  children,
+  bold = true,
+}) => {
+  return (
+    <p
+      className={cn(
+        "leading-7 [&:not(:first-child)]:mt-6",
+        bold && "font-medium"
+      )}
+    >
+      {children}
+    </p>
+  );
+};
