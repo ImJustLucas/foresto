7d2de06fd9427f88334746f865888961f703d5cd
ImJustLucas <lucas.bellier@icloud.com>
2025-02-05 15:42:28 +0100

    ✨ add CustomFormItem component for reusable form input handling with label and description

---
+++ components/forms/atoms/form-item.tsx
@@ -0,0 +1,39 @@
+import { Input } from "@/components/ui/input";
+
+type CustomFormItem<G> = {
+  name: string;
+  type: "text" | "number" | "email";
+  description: string;
+  label: string;
+  onChange: (v: G) => void;
+  value: G;
+};
+
+export const CustomFormItem = <G,>({
+  name,
+  type = "text",
+  description,
+  label,
+  onChange,
+}: CustomFormItem<G>) => {
+  return (
+    <div className="w-full">
+      <label
+        htmlFor="name"
+        className="text-sm font-semibold leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+      >
+        {label}
+      </label>
+      <Input
+        id={name}
+        name="name"
+        className="my-1"
+        type={type}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          onChange(event.target.value as unknown as G)
+        }
+      />
+      <p className="text-[0.8rem] text-muted-foreground">{description}</p>
+    </div>
+  );
+};
