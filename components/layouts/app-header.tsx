0ea19a388fbd4db07dc1ca39c509da907c0b9632
ImJustLucas <lucas.bellier@icloud.com>
2025-02-06 15:51:05 +0100

    🍱 enhance AppHeader and HomepageScreen: implement user authentication check, improve scroll behavior, and add background video to homepage

--- components/layouts/app-header.tsx
+++ components/layouts/app-header.tsx
@@ -1,9 +1,11 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { TreeDeciduous } from "lucide-react";
 import { TypographyH4 } from "../typography";
 import Link from "next/link";
 import { ROUTES } from "@/shared/constants/routes";
 import { Button } from "../ui/button";
-import { createClient } from "@/lib/supabase/supabase-server-side";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,15 +16,40 @@ import {
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { logoutAction } from "@/entities/authentication/actions/logout.actions";
+import { createSupabaseClientSide } from "@/lib/supabase/supabase-client-side";
 
-export const AppHeader: React.FC = async () => {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getUser();
+export const AppHeader: React.FC = () => {
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const [isUserNotAuthenticated, setIsUserNotAuthenticated] = useState(false);
 
-  const isUserNotAuthenticated = error || !data?.user;
+  useEffect(() => {
+    const fetchData = async () => {
+      const supabase = await createSupabaseClientSide();
+      const { data, error } = await supabase.auth.getUser();
+
+      setIsUserNotAuthenticated(Boolean(error || !data?.user));
+    };
+
+    fetchData();
+
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <header className="p-4 flex w-100 mx-auto max-w-[1280px] justify-between items-center">
+    <header
+      className={`p-4 top-2 left-0 right-0 fixed flex w-full mx-auto max-w-[1280px] justify-between items-center transition-all duration-300 ${
+        hasScrolled
+          ? "bg-white/50 backdrop-blur-md shadow-md z-10 rounded-lg"
+          : ""
+      }`}
+    >
       <Link href={ROUTES.HOMEPAGE} className="flex items-center gap-2 flex-1">
         <div className="flex items-center aspect-square size-8 space-x-2 text-white bg-green-500 rounded-full p-2">
           <TreeDeciduous className="size-4" />
--- components/screens/homepage.tsx
+++ components/screens/homepage.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export const HomepageScreen: React.FC = () => {
   return (
-    <div className="mx-auto relative max-w-[1300px]">
+    <div className="">
       {/* <div
         className="absolute top-0 left-0 w-32 h-32 rounded-full"
         style={{
@@ -15,7 +15,7 @@ export const HomepageScreen: React.FC = () => {
         }}
       /> */}
 
-      <div className="flex flex-col gap-6 items-center mt-40">
+      <div className="mx-auto relative max-w-[1300px] h-screen flex flex-col justify-center gap-6 items-center">
         <p className="uppercase bg-gradient-to-r from-green-800 to-green-300 font-bold text-8xl text-center inline-block text-transparent bg-clip-text">
           book a moment in the forest
         </p>
@@ -31,6 +31,33 @@ export const HomepageScreen: React.FC = () => {
           </Button>
           <Button asChild variant={"outline"} className="font-medium">
             <Link href={ROUTES.AUTH.REGISTER}>Register</Link>
+          </Button>
+        </div>
+      </div>
+
+      <div className="relative h-[700px] w-full overflow-hidden">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute top-0 left-0 min-w-full min-h-full object-cover z-0"
+        >
+          <source src={"/forest-video.mp4"} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <div className="absolute inset-0 bg-black bg-opacity-50 z-10"></div>
+
+        <div className="relative z-20 flex flex-col items-center justify-center h-full text-white text-center px-4">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 animate-fade-in-down">
+            Experience the magic of the forest
+          </h1>
+          <p className="text-xl md:text-2xl mb-8 max-w-2xl animate-fade-in-up">
+            Reconnect with nature and find peace within yourself.
+          </p>
+          <Button size="lg" className="animate-fade-in">
+            Create a account
           </Button>
         </div>
       </div>
