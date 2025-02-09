24f72f488dfacae43e7d88d8bd47048621a81250
ImJustLucas <lucas.bellier@icloud.com>
2025-02-09 13:21:05 +0100

    ✨ refactor DELETE API route in activities: update parameter handling to ensure proper resolution of activity ID

--- app/api/activities/[id]/route.ts
+++ app/api/activities/[id]/route.ts
@@ -61,10 +61,10 @@ export async function PUT(
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const supabase = await createClient();
-  const id = params.id;
+  const id = (await params).id;
 
   const { error } = await supabase.from("activities").delete().eq("id", id);
 
