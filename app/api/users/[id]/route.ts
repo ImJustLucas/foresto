cf610dd35bd20623d0a489550692af01a4840071
ImJustLucas <lucas.bellier@icloud.com>
2025-02-09 13:06:41 +0100

    ✨ enhance user deletion functionality: refactor DELETE API route to ensure user authentication, allow only self-deletion, and improve error handling; update delete user form to redirect after successful deletion

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
-import { createClient } from "@/lib/supabase/supabase-server-side";
+import { createClient as createServerClient } from "@/lib/supabase/supabase-server-side";
+import { createClient } from "@supabase/supabase-js";
 
 export async function PUT(
   request: Request,
   { params }: { params: Promise<{ id: string }> }
 ) {
-  const supabase = await createClient();
+  const supabase = await createServerClient();
   const id = (await params).id;
 
   const updates = await request.json();
@@ -26,35 +27,52 @@ export async function PUT(
   return NextResponse.json({ success: true, data });
 }
 
-export async function DELETE() {
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   console.log("Users - @DELETE ONE");
-  const supabase = await createClient();
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  const supabase = await createServerClient();
 
-  if (!session) {
-    return NextResponse.json({ error: "Non authentifié" }, { status: 401 });
-  }
-
-  const response = await supabase
-    .from("auth.users")
-    .delete()
-    .eq("id", session.user.id);
+  const response = await supabase.auth.getUser();
 
   if (response.error) {
-    console.error(response.error);
     return NextResponse.json(
-      { success: false, message: response.error.message },
+      { success: false, message: response.error },
+      { status: 401 }
+    );
+  } else if (!response.data) {
+    return NextResponse.json(
+      { success: false, error: "Usr not logged" },
+      { status: 401 }
+    );
+  }
+
+  const userIdToDelete = response.data.user.id;
+  const id = (await params).id;
+
+  if (id !== userIdToDelete) {
+    return NextResponse.json(
+      { error: "Action not allowed: You can only delete your own account" },
+      { status: 403 }
+    );
+  }
+
+  const adminSupabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  );
+
+  const { error } = await adminSupabase.auth.admin.deleteUser(userIdToDelete);
+
+  if (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: error.message },
       { status: 500 }
     );
   }
 
-  return NextResponse.json(
-    { success: true },
-    {
-      status: 204,
-    }
-  );
+  return NextResponse.json({ success: true }, { status: 200 });
 }
--- components/forms/delete-user.form.tsx
+++ components/forms/delete-user.form.tsx
@@ -4,17 +4,22 @@ import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { requester } from "@/lib/requester";
 import { User } from "@/shared/types/users";
+import { useRouter } from "next/navigation";
+import { ROUTES } from "@/shared/constants/routes";
 
 export const DeleteUserForm: React.FC<{
   userId: string;
 }> = ({ userId }) => {
+  const router = useRouter();
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      const response = await requester().delete<User>(`/api/users/${userId}`);
+      const response = await requester().delete<User>(`/users/${userId}`);
 
       if (response.success) {
         toast.success("User deleted successfully");
+        router.push(ROUTES.HOMEPAGE);
       } else {
         toast.error(response.message);
       }
